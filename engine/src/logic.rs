c8e5ff237282f388ad91eb94460ef83a90bc64c2
mysterious-diamond <aaronkenn10@gmail.com>
2026-07-21 21:26:48 +0700

    Give board to rust

--- engine/src/logic.rs
+++ engine/src/logic.rs
@@ -1,7 +1,18 @@
 #[repr(C)]
 pub struct Board {
-    whitePawns: u64 = 65820,
+    white_pawns: u64,
+    white_rooks: u64,
+    white_knights: u64,
+    white_bishops: u64,
+    white_queens: u64,
+    white_king: u64,
 
+    black_pawns: u64,
+    black_rooks: u64,
+    black_knights: u64,
+    black_bishops: u64,
+    black_queens: u64,
+    black_king: u64,
 }
 
 unsafe extern "C" {}
